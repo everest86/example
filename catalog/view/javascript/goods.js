df6fbb757889c73deedc866522127a0a00e37535
evere <Ledfort86*>
2025-10-31 23:17:33 +0700

    2

---
+++ catalog/view/javascript/goods.js
@@ -0,0 +1,12 @@
+const cartBtn = document.getElementById('cart-btn');
+const closeCart = document.getElementById('close-cart');
+const cartSidebar = document.getElementById('cart-sidebar');
+
+// Cart
+cartBtn.addEventListener('click', () => {
+    cartSidebar.classList.add('open');
+});
+
+closeCart.addEventListener('click', () => {
+    cartSidebar.classList.remove('open');
+});
