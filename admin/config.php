5a584ea2f2e56c72451c2b4462a54de4be3be85c
evere <Ledfort86*>
2025-11-05 01:46:00 +0700

    payment

--- admin/config.php
+++ admin/config.php
@@ -28,7 +28,7 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'MySQL-8.4');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'example');
+define('DB_DATABASE', 'firmware');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
