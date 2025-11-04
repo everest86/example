02f27f7b9d45e73789bb5765e78f06e32076917c
evere <Ledfort86*>
2025-11-05 02:27:59 +0700

    products view

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -242,6 +242,7 @@ class ControllerProductProduct extends Controller {
 			$this->document->addStyle('catalog/view/javascript/jquery/datetimepicker/bootstrap-datetimepicker.min.css');
 
 			$data['heading_title'] = $product_info['name'];
+            $data['size'] = intval($product_info['weight']);
 
 			$data['text_minimum'] = sprintf($this->language->get('text_minimum'), $product_info['minimum']);
 			$data['text_login'] = sprintf($this->language->get('text_login'), $this->url->link('account/login', '', true), $this->url->link('account/register', '', true));
